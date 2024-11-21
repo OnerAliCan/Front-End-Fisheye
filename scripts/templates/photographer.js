1a6cd4b2871102673dc9803d238d14385e517122
Ali <alican.oner.pro@gmail.com>
2024-11-21 17:35:38 +0100

    template progressing

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,11 +1,10 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement("article");
-        console.log(article);
         const img = document.createElement("img");
         img.setAttribute("src", picture);
         const h2 = document.createElement("h2");
@@ -13,6 +12,20 @@ function photographerTemplate(data) {
         console.log(name);
         article.appendChild(img);
         article.appendChild(h2);
+
+        const h3 = document.createElement("h3");
+        h3.textContent = city + ", " + country;
+        article.appendChild(h3);
+
+        const p = document.createElement("p");
+        // const br = document.createElement("<br>");
+        p.textContent = tagline;
+        article.appendChild(p);
+
+        const p2 = document.createElement("p");
+        p2.textContent = price + "€ / jour";
+        article.appendChild(p2);
+
         return article;
     }
     return { name, picture, getUserCardDOM };
