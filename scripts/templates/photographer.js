fd47059e83ac97eba7224466806b20a15169d557
Ali <alican.oner.pro@gmail.com>
2024-11-21 14:12:00 +0100

    first commit

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -4,14 +4,16 @@ function photographerTemplate(data) {
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement("article");
+        console.log(article);
+        const img = document.createElement("img");
+        img.setAttribute("src", picture);
+        const h2 = document.createElement("h2");
         h2.textContent = name;
+        console.log(name);
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, getUserCardDOM };
 }
