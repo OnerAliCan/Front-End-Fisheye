71fdbb1e5fa8c36a6a0f0a7d6a13df8a62be1676
Ali <alican.oner.pro@gmail.com>
2024-12-27 18:31:37 +0100

    restored photographerBanner.js

---
+++ scripts/components/photographerBanner.js
@@ -0,0 +1,54 @@
+function bannerTemplate(data) {
+	const { name, portrait, city, country, tagline } = data;
+	const picture = `assets/photographers/${portrait}`;
+
+	function getPhotographerCardDOM() {
+		const photographerBanner = document.querySelector(
+			".photographer-banner"
+		);
+		const photographerInformationContainer = document.createElement("div");
+
+		photographerInformationContainer.classList.add(
+			"photographer-information-container"
+		);
+
+		function generateProfilePicture() {
+			const profilePictureContainer = document.createElement("div");
+			const profilePicture = document.createElement("img");
+			profilePictureContainer.classList.add("profile-picture-container");
+			profilePicture.setAttribute("src", picture);
+			profilePicture.setAttribute("alt", name);
+
+			profilePictureContainer.appendChild(profilePicture);
+			photographerBanner.appendChild(profilePictureContainer);
+		}
+
+		function generatePhotographerName() {
+			const photographerName = document.createElement("h2");
+			photographerName.textContent = name;
+			photographerInformationContainer.appendChild(photographerName);
+			photographerBanner.appendChild(photographerInformationContainer);
+		}
+
+		function generatePhotographerLocation() {
+			const photographerLocation = document.createElement("h3");
+			photographerLocation.textContent = city + ", " + country;
+			photographerInformationContainer.appendChild(photographerLocation);
+			photographerBanner.appendChild(photographerInformationContainer);
+		}
+
+		function generatePhotographerTagline() {
+			const photographerTagline = document.createElement("p");
+			photographerTagline.textContent = tagline;
+			photographerInformationContainer.appendChild(photographerTagline);
+			photographerBanner.appendChild(photographerInformationContainer);
+		}
+
+		generatePhotographerName();
+		generatePhotographerLocation();
+		generatePhotographerTagline();
+		generateProfilePicture();
+		return photographerBanner;
+	}
+	return { getPhotographerCardDOM };
+}
